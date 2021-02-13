cf6b599d3064f0f56834a2ae2881f6fa615cc411
Gökberk Deprem <75221412+gokberkdeprem@users.noreply.github.com>
2021-02-13 14:16:41 +0300

    Fix thumbnails and avatar

--- src/components/AuthModal/index.js
+++ src/components/AuthModal/index.js
@@ -3,72 +3,68 @@ import { Modal, Button, Header, Icon } from "semantic-ui-react";
 import Box from "../Box";
 
 function AuthModal({ children, complete, cb }) {
-	const [myFile, setFile] = useState(undefined);
-	const [open, setOpen] = React.useState(false);
-	return (
-		<Modal
-			onClose={() => setOpen(false)}
-			onOpen={() => setOpen(true)}
-			closeIcon
-			open={open}
-			trigger={
-				<button
-					style={{
-						display: "flex",
-						borderWidth: 1,
-						padding: 20,
-						margin: 5,
-						alignContent: "center",
-						justifyContent: "center",
-						flex: 1,
-						border: "dotted",
-						borderColor: complete === true ? "green" : "red",
-						backgroundColor: "#fff",
-					}}
-				>
-					<Box complete={complete}>{children}</Box>
-				</button>
-			}
-			header={children}
-			actions={[
-				"İptal",
-				{ key: "done", content: "Gönder", positive: true },
-			]}
-		>
-			{" "}
-			<Header icon="archive" content={children} />
-			<Modal.Content>
-				<div>
-					<input
-						type={"file"}
-						style={{ width: "100%" }}
-						onChange={(a) => {
-							const file = Array.prototype.slice.call(
-								a.target.files
-							);
-							setFile(file);
-						}}
-					/>
-				</div>
-			</Modal.Content>
-			<Modal.Actions>
-				<Button color="red" onClick={() => setOpen(false)}>
-					<Icon name="remove" /> No
-				</Button>
-				<Button
-					color="green"
-					onClick={() => {
-						if (myFile) {
-							cb(myFile);
-						}
-						setOpen(false);
-					}}
-				>
-					<Icon name="checkmark" /> Yes
-				</Button>
-			</Modal.Actions>
-		</Modal>
-	);
+  const [myFile, setFile] = useState(undefined);
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      closeIcon
+      open={open}
+      trigger={
+        <button
+          style={{
+            display: "flex",
+            borderWidth: 1,
+            padding: 20,
+            margin: 5,
+            alignContent: "center",
+            justifyContent: "center",
+            flex: 1,
+            border: "black",
+            broder: "white",
+            borderColor: complete === true ? "green" : "red",
+            backgroundColor: "#1890FF",
+          }}
+        >
+          <Box complete={complete}>{children}</Box>
+        </button>
+      }
+      header={children}
+      actions={["İptal", { key: "done", content: "Gönder", positive: true }]}
+    >
+      {" "}
+      <Header icon="archive" content={children} />
+      <Modal.Content>
+        <div>
+          <input
+            type={"file"}
+            style={{ width: "100%" }}
+            onChange={(a) => {
+              const file = Array.prototype.slice.call(a.target.files);
+              setFile(file);
+            }}
+          />
+        </div>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="red" onClick={() => setOpen(false)}>
+          <Icon name="remove" /> No
+        </Button>
+        <Button
+          color="green"
+          onClick={() => {
+            if (myFile) {
+              cb(myFile);
+            }
+            setOpen(false);
+          }}
+        >
+          <Icon name="checkmark" /> Yes
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
 export default AuthModal;
