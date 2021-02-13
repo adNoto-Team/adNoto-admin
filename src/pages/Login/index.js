3a7f89db82e76a3298b8725edcf63819e0e3f70f
Gökberk Deprem <75221412+gokberkdeprem@users.noreply.github.com>
2021-02-13 13:58:59 +0300

    Change general look

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -12,122 +12,110 @@ import Button from "../../components/Button";
 import Context from "../../context/context";
 
 const Username = ({ cb }) => {
-	return (
-		<Input
-			className={styles.input}
-			onChange={cb}
-			size="large"
-			placeholder="Enter Username"
-			prefix={<UserOutlined />}
-			style={{
-				borderRadius: 20,
-				padding: 15,
-				fontSize: 20,
-				fontWeight: "700",
-			}}
-		/>
-	);
+  return (
+    <Input
+      className={styles.input}
+      onChange={cb}
+      size="large"
+      placeholder="Enter Username"
+      prefix={<UserOutlined />}
+      style={{
+        borderRadius: 20,
+        padding: 15,
+        fontSize: 20,
+        fontWeight: "700",
+      }}
+    />
+  );
 };
 
 const Password = ({ cb }) => {
-	return (
-		<Input.Password
-			className={styles.input}
-			size={"large"}
-			style={{
-				borderRadius: 20,
-				padding: 15,
-				fontSize: 20,
-				fontWeight: "700",
-			}}
-			placeholder="input password"
-			iconRender={(visible) =>
-				visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-			}
-			onChange={cb}
-		/>
-	);
+  return (
+    <Input.Password
+      className={styles.input}
+      size={"large"}
+      style={{
+        borderRadius: 20,
+        padding: 15,
+        fontSize: 20,
+        fontWeight: "700",
+      }}
+      placeholder="input password"
+      iconRender={(visible) =>
+        visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+      }
+      onChange={cb}
+    />
+  );
 };
 
 const Login = () => {
-	let history = useHistory();
-	const { login, error, setError } = useContext(Context);
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  let history = useHistory();
+  const { login, error, setError } = useContext(Context);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	return (
-		<div className={styles.mainContainer}>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-				}}
-			>
-				<div className={styles.container}>
-					<Username
-						cb={(e) => {
-							setUsername(
-								e.target.value.replace(/[^A-Za-z]/gi, "")
-							);
-						}}
-					/>
-				</div>
-				<div className={styles.container}>
-					<Password
-						cb={(e) => {
-							setPassword(
-								e.target.value.replace(/[^a-zA-Z0-9 ]/gi, "")
-							);
-						}}
-					/>
-				</div>
-				<div
-					className={styles.container}
-					style={{
-						marginTop: 5,
-						display: "flex",
-						alignContent: "center",
-						justifyContent: "center",
-						fontWeight: "900",
-					}}
-				>
-					<Button
-						event={() => {
-							if (username === "admin") {
-								login(username, password, () => {
-									setError("");
-									history.push("/");
-								});
-							} else {
-								setError("You are not admin!");
-							}
-						}}
-					>
-						<div
-							style={{
-								fontWeight: "900",
-								fontSize: 20,
-								paddingRight: 80,
-								paddingLeft: 80,
-							}}
-						>
-							Login
-						</div>
-					</Button>
-				</div>
-			</form>
+  return (
+    <div className={styles.mainContainer}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className={styles.container}>
+          <Username
+            cb={(e) => {
+              setUsername(e.target.value.replace(/[^A-Za-z]/gi, ""));
+            }}
+          />
+        </div>
+        <div className={styles.container}>
+          <Password
+            cb={(e) => {
+              setPassword(e.target.value.replace(/[^a-zA-Z0-9 ]/gi, ""));
+            }}
+          />
+        </div>
+        <div
+          className={styles.container}
+          style={{
+            marginTop: 5,
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center",
+            fontWeight: "900",
+          }}
+        >
+          <Button
+            style={{ color: "black" }}
+            event={() => {
+              if (username === "admin") {
+                login(username, password, () => {
+                  setError("");
+                  history.push("/");
+                });
+              } else {
+                setError("You are not admin!");
+              }
+            }}
+          >
+            <div>Login</div>
+          </Button>
+        </div>
+      </form>
 
-			<div
-				style={{
-					backgroundColor: "#f26b71",
-					borderRadius: "10%",
-					display: "flex",
-					justifyContent: "center",
-				}}
-			>
-				{error}
-			</div>
-		</div>
-	);
+      <div
+        style={{
+          backgroundColor: "#f26b71",
+          borderRadius: "10%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {error}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 import Container from "../../components/Container";
-import Button from "../../components/Button";
 
 const Payment = () => {
-	return (
-		<div>
-			<Container>
-				<h3>Kredi Kartı</h3>
-				Henüz banka / kredi kartı bilgilerinizi bizimle paylaşmadınız!
-				<br />
-				<br />
-				<Button>Ödeme Yöntemi Ekle</Button>
-				<hr />
-			</Container>
-		</div>
-	);
+  return (
+    <div>
+      <Container>
+        <h3>Admin Panel User Settings</h3>
+        <h4>These features will be added soon!</h4>
+        <hr />
+        <ul>
+          <li>User Authorization</li>
+          <li>User Photo Control</li>
+          <li>User Access</li>
+          <li>User Statistics</li>
+        </ul>
+      </Container>
+    </div>
+  );
 };
 
 export default Payment;
